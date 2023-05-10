e6832aa8657a45d0816760bfc613a30421216e4e
RKnagre <nagrek205@gmail.com>
2023-05-10 13:07:30 +0530

    codepush

---
+++ src/main/java/com/example/mintra/MintraApplication.java
@@ -0,0 +1,13 @@
+package com.example.mintra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MintraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mintra/MintraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mintra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
